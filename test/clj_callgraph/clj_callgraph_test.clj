6f65a51cc951496b467f4ac63a712016a89bd658
Shogo Ohta <athos0220@gmail.com>
2022-09-23 23:48:02 +0900

    first commit

---
+++ test/clj_callgraph/clj_callgraph_test.clj
@@ -0,0 +1,7 @@
+(ns clj-callgraph.clj-callgraph-test
+  (:require [clojure.test :refer :all]
+            [clj-callgraph.clj-callgraph :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
