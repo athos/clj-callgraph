4688a0ae6a86895c993eb6779edc316c46ab9516
Shogo Ohta <athos0220@gmail.com>
2022-10-04 00:32:00 +0900

    Rearrange API

--- src/clj_callgraph/api.clj
+++ src/clj_callgraph/api.clj
@@ -2,12 +2,40 @@
   (:require [clj-callgraph.analysis :as ana]
             [clj-callgraph.diff :as diff]
             [clj-callgraph.renderer :as render]
-            [clojure.edn :as edn]))
+            [clojure.edn :as edn]
+            [clojure.java.io :as io])
+  (:import [java.io PushbackReader]))
 
-(defn var-graph [& filenames]
-  (render/render (ana/analyze filenames)))
+(defn- with-out [{:keys [out]} f]
+  (if out
+    (with-open [w (io/writer out)]
+      (binding [*out* w]
+        (f)))
+    (f)))
 
-(defn diff-var-graph [file1 file2]
-  (let [deps1 (edn/read-string (slurp file1))
-        deps2 (edn/read-string (slurp file2)) ]
-    (render/render (diff/build-diff-deps deps1 deps2))))
+(defn dump-deps
+  ([src-files] (dump-deps src-files {}))
+  ([src-files opts]
+   (with-out opts #(prn (ana/analyze src-files)))))
+
+(defn- read-dump-file [dump-file]
+  (with-open [r (PushbackReader. (io/reader dump-file))]
+    (edn/read r)))
+
+(defn render-graph
+  ([dump-file] (render-graph dump-file {}))
+  ([dump-file opts]
+   (with-out opts #(render/render (read-dump-file dump-file)))))
+
+(defn render-diff-graph
+  ([dump-file1 dump-file2]
+   (render-diff-graph dump-file1 dump-file2 {}))
+  ([dump-file1 dump-file2 opts]
+   (let [deps1 (read-dump-file dump-file1)
+         deps2 (read-dump-file dump-file2)]
+     (with-out opts #(render/render (diff/build-diff-deps deps1 deps2))))))
+
+(defn var-graph
+  ([src-files] (var-graph src-files {}))
+  ([src-files opts]
+   (with-out opts #(render/render (ana/analyze src-files)))))
