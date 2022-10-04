7725b576418c3effe5a4f9b3511a804eda1b4382
Shogo Ohta <athos0220@gmail.com>
2022-10-04 23:11:41 +0900

    Allow to pass file names as symbol

--- src/clj_callgraph/cli.clj
+++ src/clj_callgraph/cli.clj
@@ -2,16 +2,19 @@
   (:require [clj-callgraph.api :as api]
             [clojure.java.io :as io]))
 
+(defn- coerce-out [{:keys [out] :as opts}]
+  (cond-> opts out (update :out str)))
+
 (defn dump-data [opts]
   (with-open [r (io/reader *in*)]
-    (api/dump-data (line-seq r) opts)))
+    (api/dump-data (line-seq r) (coerce-out opts))))
 
 (defn render-graph [{:keys [dump] :as opts}]
-  (api/render-graph dump opts))
+  (api/render-graph (str dump) (coerce-out opts)))
 
 (defn render-diff-graph [{:keys [dump1 dump2] :as opts}]
-  (api/render-diff-graph dump1 dump2 opts))
+  (api/render-diff-graph dump1 dump2 (coerce-out opts)))
 
 (defn generate-graph [opts]
   (with-open [r (io/reader *in*)]
-    (api/generate-graph (line-seq r) opts)))
+    (api/generate-graph (line-seq r) (coerce-out opts))))
