999fc6bf673ee280a0333fc834f97fb34072096a
Shogo Ohta <athos0220@gmail.com>
2022-10-09 23:32:31 +0900

    Tweak some opts for cli

--- src/clj_callgraph/cli.clj
+++ src/clj_callgraph/cli.clj
@@ -9,11 +9,11 @@
   (with-open [r (io/reader *in*)]
     (api/dump-data (line-seq r) (coerce-out opts))))
 
-(defn render-graph [{:keys [dump] :as opts}]
-  (api/render-graph (str dump) (coerce-out opts)))
+(defn render-graph [{:keys [in] :as opts}]
+  (api/render-graph (str in) (coerce-out opts)))
 
-(defn render-diff-graph [{:keys [dump1 dump2] :as opts}]
-  (api/render-diff-graph dump1 dump2 (coerce-out opts)))
+(defn render-diff-graph [{:keys [in1 in2] :as opts}]
+  (api/render-diff-graph (str in1) (str in2) (coerce-out opts)))
 
 (defn generate-graph [opts]
   (with-open [r (io/reader *in*)]
