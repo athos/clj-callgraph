ef4c3b3e77deb258e505a79ff5ad0e1c580dcb68
Shogo Ohta <athos0220@gmail.com>
2022-10-10 14:29:16 +0900

    Refactor DotRenderer impl

--- src/clj_callgraph/renderer/dot.clj
+++ src/clj_callgraph/renderer/dot.clj
@@ -12,53 +12,60 @@
     (if (nil? y) 0 -1)
     (if (nil? y) 1 (compare x y))))
 
-(def ^:dynamic *output*)
+(defn- render-node [output k attrs]
+  (output/printf output
+                 "%s[label=\"%s\" %s];\n" (munge* k) (:name attrs)
+                 (cond (:added (:status attrs))
+                       "color=chartreuse2 style=bold"
 
-(defn- println [x]
-  (output/println *output* x))
+                       (:removed (:status attrs))
+                       "color=crimson style=\"bold,dashed\""
 
-(defn- printf [fmt & args]
-  (apply output/printf *output* fmt args))
+                       :else "")))
+
+(defn- render-namespace
+  [output ns-name [[_ {:keys [ns-added ns-removed]}] :as entries]]
+  (output/printf output
+                 "subgraph cluster_%s {\nshape=\"rect\";\nlabel=\"%s\";\n%s"
+                 (munge* ns-name) ns-name
+                 (cond ns-added "color=chartreuse2 style=bold;\n"
+                       ns-removed "color=crimson style=bold\n"
+                       :else ""))
+  (doseq [[k attrs] (sort safe-id-comparator entries)]
+    (render-node output k attrs))
+  (output/println output "}"))
+
+(defn- render-namespaces [output entries-by-ns]
+  (doseq [[ns entries] entries-by-ns]
+    (render-namespace output ns entries)))
+
+(defn- render-arrows [output entries-by-ns]
+  (doseq [[_ entries] entries-by-ns
+          [k attrs] (sort entries)
+          k' (:deps attrs)]
+    (output/printf output
+                   "%s -> %s %s;\n" (munge* k) (munge* k')
+                   (let [e (get (:edges attrs) k')]
+                     (cond (or (:added (:status attrs)) (:added e))
+                           "[color=chartreuse2 style=bold]"
+
+                           (or (:removed (:status attrs)) (:removed e))
+                           "[color=crimson style=\"bold,dashed\"]"
+
+                           :else "")))))
 
 (defrecord DotRenderer [output opts]
   proto/IRenderer
   (render [_ deps]
-    (binding [*output* output]
-      (println "digraph G {")
-      (println "rankdir=\"LR\";")
-      (println "fontname=\"monospace\";")
-      (println "node[fontname=\"monospace\",ordering=\"in\"];")
-      (let [entries-by-ns (group-by (comp :ns val) deps)]
-        (doseq [[ns [[_ {:keys [ns-added ns-removed]}]]] entries-by-ns]
-          (printf "subgraph cluster_%s {\nshape=\"rect\";\nlabel=\"%s\";\n%s"
-                  (munge* ns) ns
-                  (cond ns-added "color=chartreuse2 style=bold;\n"
-                        ns-removed "color=crimson style=bold\n"
-                        :else ""))
-          (doseq [[k attrs] (->> (get entries-by-ns ns)
-                                 (sort safe-id-comparator))]
-            (printf "%s[label=\"%s\" %s];\n" (munge* k) (:name attrs)
-                    (cond (:added (:status attrs))
-                          "color=chartreuse2 style=bold"
-
-                          (:removed (:status attrs))
-                          "color=crimson style=\"bold,dashed\""
-
-                          :else "")))
-          (println "}"))
-        (doseq [[_ entries] entries-by-ns
-                [k attrs] (sort entries)
-                k' (:deps attrs)]
-          (printf "%s -> %s %s;\n" (munge* k) (munge* k')
-                  (let [e (get (:edges attrs) k')]
-                    (cond (or (:added (:status attrs)) (:added e))
-                          "[color=chartreuse2 style=bold]"
-
-                          (or (:removed (:status attrs)) (:removed e))
-                          "[color=crimson style=\"bold,dashed\"]"
-
-                          :else ""))))
-        (println "}")))))
+    (doto output
+      (output/println "digraph G {")
+      (output/println "rankdir=\"LR\";")
+      (output/println "fontname=\"monospace\";")
+      (output/println "node[fontname=\"monospace\",ordering=\"in\"];"))
+    (let [entries-by-ns (group-by (comp :ns val) deps)]
+      (render-namespaces output entries-by-ns)
+      (render-arrows output entries-by-ns))
+    (output/println output "}")))
 
 (defn make-dot-renderer [output opts]
   (->DotRenderer output opts))
