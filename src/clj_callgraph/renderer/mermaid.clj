52055788aafd1c850f73f443a4aa0fd27819bc7b
Shogo Ohta <athos0220@gmail.com>
2022-10-12 00:55:52 +0900

    Tweak node IDs to work around Mermaid's limitation

--- src/clj_callgraph/renderer/mermaid.clj
+++ src/clj_callgraph/renderer/mermaid.clj
@@ -4,17 +4,22 @@
             [clj-callgraph.protocols :as proto]
             [clj-callgraph.renderer.utils :as utils]))
 
+(defn- munge* [s]
+  ;; current mermaid doesn't allow node ids ending with a keyword (e.g. a_graph)
+  ;; so modify ids here to always end with `_`
+  (str (utils/munge* s) \_))
+
 (defn- render-namespaces [output deps]
   (let [entries-by-ns (group-by (comp :ns val) deps)]
     (doseq [ns (ns/topo-sorted-namespaces deps)]
       (when ns
         (output/printf output "subgraph \"%s\"\n" ns))
       (doseq [[k attrs] (utils/sort-by-id (get entries-by-ns ns))]
-        (output/printf output "%s([\"%s\"])\n" (utils/munge* k) (:name attrs))
+        (output/printf output "%s([\"%s\"])\n" (munge* k) (:name attrs))
         (doseq [k' (:deps attrs)]
           (output/printf output "%s --> %s\n"
-                         (utils/munge* k)
-                         (utils/munge* k'))))
+                         (munge* k)
+                         (munge* k'))))
       (when ns
         (output/println output "end")))))
 
