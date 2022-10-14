56da9a576624ee8a5124b8cba567832561e714a2
Shogo Ohta <athos0220@gmail.com>
2022-10-15 00:18:29 +0900

    Rename option to specify renderer from :format to :renderer

--- src/clj_callgraph/renderer.clj
+++ src/clj_callgraph/renderer.clj
@@ -3,13 +3,13 @@
             [clj-callgraph.renderer.dot :as dot]
             [clj-callgraph.renderer.mermaid :as mermaid]))
 
-(defmulti make-renderer (fn [_output opts] (keyword (:format opts))))
+(defmulti make-renderer (fn [_output opts] (keyword (:renderer opts))))
 
 (defmethod make-renderer :default [output opts]
-  (if-let [fmt (:format opts)]
-    (throw (ex-info (str "Unknown format specified: " (name fmt))
-                    {:format fmt}))
-    (make-renderer output (assoc opts :format :dot))))
+  (if-let [fmt (:renderer opts)]
+    (throw (ex-info (str "Unknown renderer specified: " (name fmt))
+                    {:renderer fmt}))
+    (make-renderer output (assoc opts :renderer :dot))))
 
 (defmethod make-renderer :dot [output opts]
   (dot/make-dot-renderer output opts))
