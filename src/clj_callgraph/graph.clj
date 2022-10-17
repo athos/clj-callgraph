61f2dbee33b44eb9c5e4c19c1f1f8bc7e709608c
Shogo Ohta <athos0220@gmail.com>
2022-10-17 23:44:11 +0900

    Keep nodes with no dependencies after applying transpose-graph

--- src/clj_callgraph/graph.clj
+++ src/clj_callgraph/graph.clj
@@ -2,8 +2,9 @@
 
 (defn transpose-graph [graph]
   (reduce-kv (fn [ret k {:keys [deps]}]
-               (reduce #(update-in %1 [%2 :deps] (fnil conj #{}) k) ret deps))
-             {} graph))
+               (reduce #(update-in %1 [%2 :deps] conj k) ret deps))
+             (into {} (map (fn [k] [k {:deps #{}}]) (keys graph)))
+             graph))
 
 (defn topo-sort [graph]
   (loop [ret ()
